6abaf258a539c06e5398b946756cfea1ecd7698b
Nate Wessel <nate@natewessel.com>
2025-08-18 21:10:42 -0400

    change to canadian phone number

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
-	<footer>Contact: <a href='mailto:jeremy@meddlingwithnature.com'>jeremy@meddlingwithnature.com</a> | 1-513-666-8867</footer>
+	<footer>Contact: <a href='mailto:jeremy@meddlingwithnature.com'>jeremy@meddlingwithnature.com</a> | 1-437-605-3581</footer>
 <?php wp_footer(); ?>
 </body>
 </html>
-
 
